2c63856f1ce63639d67ecf2b2d6e873e94a1fa1e
SarkarManisha <73577093+SarkarManisha@users.noreply.github.com>
2022-02-18 17:07:50 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -11,7 +11,6 @@ let engine;
 let world;
 var rope,fruit,ground;
 var fruit_con;
-var fruit_con_2;
 
 var bg_img;
 var food;
@@ -19,22 +18,13 @@ var rabbit;
 
 var button;
 var bunny;
-var blink,eat,sad;
 
 function preload()
 {
   bg_img = loadImage('background.png');
   food = loadImage('melon.png');
   rabbit = loadImage('Rabbit-01.png');;
-  blink = loadAnimation("blink_1.png","blink_2.png","blink_3.png");
-  eat = loadAnimation("eat_0.png" , "eat_1.png","eat_2.png","eat_3.png","eat_4.png");
-  sad = loadAnimation("sad_1.png","sad_2.png","sad_3.png");
-  
-  blink.playing = true;
-  eat.playing = true;
-  sad.playing = true;
-  sad.looping= false;
-  eat.looping = false; 
+ 
 }
 
 function setup() {
@@ -49,20 +39,14 @@ function setup() {
   button.size(50,50);
   button.mouseClicked(drop);
   
-
-  blink.frameDelay = 20;
-  eat.frameDelay = 20;
-  bunny = createSprite(230,620,100,100);
-  bunny.scale = 0.2;
-
-  bunny.addAnimation('blinking',blink);
-  bunny.addAnimation('eating',eat);
-  bunny.addAnimation('crying',sad);
-  bunny.changeAnimation('blinking');
-  
   rope = new Rope(7,{x:245,y:30});
   ground = new Ground(200,690,600,20);
-  
+
+
+  bunny = createSprite(230,620,100,100);
+  bunny.addImage(rabbit);
+  bunny.scale = 0.2;
+
   fruit = Bodies.circle(300,300,20);
   Matter.Composite.add(rope.body,fruit);
 
@@ -91,7 +75,6 @@ function draw()
 function drop()
 {
   rope.break();
-  fruit_con.detach();
+  fruit_con.dettach();
   fruit_con = null; 
 }
-
